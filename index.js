4390aced7ae016bbc6d3df1a236a6a9ca2c86be5
Rishabh <rishabh010189@gmail.com>
2023-04-24 16:13:05 +0530

    docker compose with redis server

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const redis = require('redis');
 
 const app = express();
+const client = redis.createClient({
+    host: 'redis-server',
+    port: 6379
+});
+client.set('visits', 0);
+
 
 app.get('/', (req, res) => {
-    res.statusCode = 200;
-    res.send('hello from express');
+    client.get('visits', (err, visits) =>{
+        res.statusCode = 200;
+        res.send(`hello from express, you are visitor number ${visits} `);
+        client.set('visits', parseInt(visits) + 1);
+    })
 });
 
 app.listen(8080, () => {
